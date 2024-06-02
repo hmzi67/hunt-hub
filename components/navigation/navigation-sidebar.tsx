b27c1dc59c807427439d39403dd6c26c0989622b
hmzi67 <hamzawaheed057@gmail.com>
2024-06-03 01:29:19 +0500

    logout funtionality and dark mode toggle added

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,4 +1,5 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -8,6 +9,7 @@ import { db } from "@/lib/db";
 
 import { NavigationAction } from "./navigation-action";
 import { NavigationItem } from "./navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -40,6 +42,19 @@ export const NavigationSidebar = async () => {
                     </div>
                 ))}
             </ScrollArea>
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle />
+                <UserButton
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements: {
+                            avatarBox: "h-[48px] w-[48px]",
+                        }
+                    }}
+                
+                />
+
+            </div>
         </div>
     );
 };
