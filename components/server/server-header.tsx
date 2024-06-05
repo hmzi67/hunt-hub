0be8b3c1d3f3440935e4b18232c6327dc5fe7704
hmzi67 <hamzawaheed057@gmail.com>
2024-06-06 02:29:48 +0500

    Adding Invite friends functionality in channel

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -19,6 +19,7 @@ import {
     DropdownMenuSeparator, 
     DropdownMenuTrigger 
 } from "@/components/ui/dropdown-menu";
+import { useModal } from "@/hooks/use-modal-store";
 
 
 interface ServerHeaderProps {
@@ -30,6 +31,7 @@ export const ServerHeader = ({
     server,
     role
 }: ServerHeaderProps ) => {
+    const { onOpen } = useModal()
     const isAdmin = role === MemberRole.ADMIN;
     const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -49,6 +51,7 @@ export const ServerHeader = ({
             >
             {isModerator && (
                 <DropdownMenuItem
+                    onClick={() => onOpen("invite", { server })}
                     className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
                 >
                     Invite People
