d48ac6242b6d5896d656513d0ec6902431bd8bb8
hmzi67 <hamzawaheed057@gmail.com>
2024-08-01 03:10:41 +0500

    Now we can edit and delete channels

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -18,6 +18,7 @@ import { useParams, useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
     channel: Channel;
@@ -36,6 +37,7 @@ export const ServerChannel = ({
     server,
     role,
 }: ServerChannelProps) => {
+    const { onOpen } = useModal();
     const params = useParams();
     const router = useRouter();
 
@@ -61,16 +63,20 @@ export const ServerChannel = ({
             {channel.name !== "general" && role !== MemberRole.GUEST && (
                 <div className="ml-auto flex items-center gap-x-2">
                     <ActionTooltip label="Edit" >
-                        <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+                        <Edit 
+                            onClick={() => onOpen("editChannel", { server, channel })}
+                            className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
                     </ActionTooltip>
-                    <ActionTooltip label="Edit" >
-                        <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+                    <ActionTooltip label="Delete" >
+                        <Trash 
+                            onClick={() => onOpen("deleteChannel", { server, channel })}
+                            className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
                     </ActionTooltip>
                 </div>
             )}
             {channel.name === "general" && (
                 <Lock
-                className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400" 
+                    className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400" 
                 />
             )}
         </div>
