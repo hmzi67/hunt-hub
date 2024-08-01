a1611307947c3b1c8d18d3189c0a49c7fb9d4a84
hmzi67 <hamzawaheed057@gmail.com>
2024-08-01 05:08:31 +0500

    created chat header with navigation toggle

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,6 +1,53 @@
-const ChannelIdPage = () => {
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+
+import { currentProfile } from "@/lib/current-profile";
+import { ChatHeader } from "@/components/chat/chat-header";
+import { db } from "@/lib/db";
+
+
+interface ChannelIdPageProps {
+    params: {
+        serverId: string;
+        channelId: string;
+    }
+}
+
+const ChannelIdPage = async ({
+    params
+}: ChannelIdPageProps ) => {
+
+    const profile = await currentProfile()
+
+    if (!profile) {
+        return auth().redirectToSignIn();
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        },
+    });
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id
+        }
+    });
+
+    if (!channel || !member) {
+        redirect("/");
+    }
+
     return ( 
-        <div>Channel Id Page</div>
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader 
+                serverId={channel.serverId} 
+                name={channel.name} 
+                type="channel"        
+        />
+        </div>
     );
 }
  
