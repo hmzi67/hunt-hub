a15a5ffbfee41e5fa3d24fe5009b72dc8fe3c890
hmzi67 <hamzawaheed057@gmail.com>
2024-08-06 15:58:39 +0500

    Update Chat Item UI and also add edit and delete message functionality

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -15,7 +15,7 @@ export const UserAvatar = ({
             "h-7 w-7 md:h-10 md:w-10 rounded-full",
             className
         )}>
-            <AvatarImage src={src} className="rounded-full object-cover" />
+            <AvatarImage src={src} className="h-7 w-7 md:h-10 md:w-10 rounded-full object-cover" />
         </Avatar>
     )
 }
